5f7777213476c53a4f5c01f6e8be5fcb639e8e64
ashen <jayasinghe.ashen@gmail.com>
2018-01-29 21:10:43 +0530

    First Commit

---
+++ src/main/java/com/example/demo/TravisCiProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiProjectApplication.class, args);
+	}
+}
